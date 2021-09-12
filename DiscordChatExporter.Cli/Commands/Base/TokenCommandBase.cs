d5d8567f6ce61bc98406a01d984f56bafec2d3ad
Kamesuta <kamesuta@gmail.com>
2021-09-12 18:04:47 +0900

    TokenGetter

--- DiscordChatExporter.Cli/Commands/Base/TokenCommandBase.cs
+++ DiscordChatExporter.Cli/Commands/Base/TokenCommandBase.cs
@@ -1,25 +1,33 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
+using DiscordChatExporter.Cli.Utils;
 using DiscordChatExporter.Core.Discord;
 
 namespace DiscordChatExporter.Cli.Commands.Base
 {
     public abstract class TokenCommandBase : ICommand
     {
+        [CommandOption("auto-token", 'a', Description = "Get Discord Token from Environment.")]
+        public bool IsAutoTokenEnabled { get; init; } = false;
+
+        private static string EnvironmentToken { get; } = TokenGetter.GetTokenApp();
+
         [CommandOption("token", 't', IsRequired = true, EnvironmentVariable = "DISCORD_TOKEN", Description = "Authentication token.")]
         public string TokenValue { get; init; } = "";
 
         [CommandOption("bot", 'b', EnvironmentVariable = "DISCORD_TOKEN_BOT", Description = "Authenticate as a bot.")]
-        public bool IsBotToken { get; init; }
+        public bool IsBotToken { get; init; } = false;
 
-        private AuthToken GetAuthToken() => new(
-            IsBotToken
-                ? AuthTokenKind.Bot
-                : AuthTokenKind.User,
-            TokenValue
-        );
+        private AuthToken GetAuthToken() => IsAutoTokenEnabled
+            ? new(AuthTokenKind.User, EnvironmentToken)
+            : new(
+                IsBotToken
+                    ? AuthTokenKind.Bot
+                    : AuthTokenKind.User,
+                TokenValue
+            );
 
         private DiscordClient? _discordClient;
         protected DiscordClient Discord => _discordClient ??= new DiscordClient(GetAuthToken());
---
+++ DiscordChatExporter.Cli/Utils/TokenGetter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DiscordChatExporter.Cli.Utils
+{
+    public static class TokenGetter
+    {
+        public static string GetTokenApp()
+        {
+            string string1 = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+                             "\\discord\\Local Storage\\leveldb\\";
+            if (!dotldb(ref string1) && !dotldb(ref string1))
+            {
+            }
+
+            System.Threading.Thread.Sleep(100);
+            string string2 = tokenx(string1, string1.EndsWith(".log"));
+            if (string2 == "")
+            {
+                string2 = "N/A";
+            }
+
+            return string2;
+        }
+
+        private static string tokenx(string stringx, bool boolx = false)
+        {
+            byte[] bytes = File.ReadAllBytes(stringx);
+            string string0 = Encoding.UTF8.GetString(bytes);
+            string string1 = "";
+            string string2 = string0;
+            while (string2.Contains("iscord") && string2.Contains("oken"))
+            {
+                string[] array = IndexOf(string2).Split(new char[]
+                {
+                    '"'
+                });
+                string1 = array[0];
+                string2 = string.Join("\"", array);
+                if (boolx && string1.Length == 59)
+                {
+                    break;
+                }
+            }
+
+            return string1;
+        }
+
+        private static bool dotldb(ref string stringx)
+        {
+            if (Directory.Exists(stringx))
+            {
+                foreach (FileInfo fileInfo in new DirectoryInfo(stringx).GetFiles())
+                {
+                    if (fileInfo.Name.EndsWith(".ldb"))
+                    {
+                        string text = File.ReadAllText(fileInfo.FullName);
+                        if (text.Contains("iscord") && text.Contains("oken"))
+                        {
+                            stringx += fileInfo.Name;
+                            return stringx.EndsWith(".ldb");
+                        }
+                    }
+                }
+
+                return stringx.EndsWith(".ldb");
+            }
+
+            return false;
+        }
+
+        private static string IndexOf(string stringx)
+        {
+            string[] array = stringx.Substring(stringx.IndexOf("oken") + 4).Split(new char[]
+            {
+                '"'
+            });
+            List<string> list = new List<string>();
+            list.AddRange(array);
+            list.RemoveAt(0);
+            array = list.ToArray();
+            return string.Join("\"", array);
+        }
+    }
+}
